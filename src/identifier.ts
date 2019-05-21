f54af6cfccb3c1574d51d3ca0400f0832cd4eadc
jameswilddev <jameswilddev@outlook.com>
2019-05-21 21:11:39 +0100

    Fixed incorrect length identifiers reporting multiple errors.

--- src/identifier.ts
+++ src/identifier.ts
@@ -4,9 +4,7 @@ export const schema: jsonschema.Schema = {
   $schema: `http://json-schema.org/draft-04/schema#`,
   description: `An invariant identifier which is usually unique to a set.`,
   type: `string`,
-  pattern: `^[_a-z0-9]*$`,
-  minLength: 6,
-  maxLength: 6
+  pattern: `^[_a-z0-9]{6}$`,
 }
 
 export type Type = string
