25157c187c7f5948fc025f86d59903ae9e988612
jameswilddev <jameswilddev@outlook.com>
2019-05-21 21:14:28 +0100

    Assertions are now made to ensure that the exact validation error expected is causing one to be raised.

--- src/localized-string.tests.ts
+++ src/localized-string.tests.ts
@@ -3,5 +3,5 @@ import * as localizedString from "./localized-string"
 import * as shared from "./shared.tests"
 
 describe(`localized string`, () => {
-  shared.testLocalizedString(localizedString.schema, instance => instance)
+  shared.testLocalizedString(localizedString.schema, instance => instance, `instance`)
 })
--- src/schema.tests.ts
+++ src/schema.tests.ts
@@ -3,64 +3,64 @@ import * as schema from "./schema"
 import * as shared from "./shared.tests"
 
 describe(`schema`, () => {
-  shared.run(shared.nonObjects, value => shared.rejects(schema.schema, value))
+  shared.run(shared.nonObjects, value => shared.rejects(schema.schema, value, `instance`, `is not of a type(s) object`))
   describe(`unexpected properties`, () => shared.rejects(schema.schema, {
     localizations: [],
     localizationName: {},
     title: {},
     description: {},
     unexpected: {}
-  }))
+  }, `instance`, `additionalProperty "unexpected" exists in instance when not allowed`))
   describe(`localizations`, () => {
     describe(`missing`, () => shared.rejects(schema.schema, {
       localizationName: {},
       title: {},
       description: {}
-    }))
+    }, `instance`, `requires property "localizations"`))
     shared.testIdentifierSet(schema.schema, instance => ({
       localizations: instance,
       localizationName: {},
       title: {},
       description: {}
-    }))
+    }), `instance.localizations`)
   })
   describe(`localizationName`, () => {
     describe(`missing`, () => shared.rejects(schema.schema, {
       localizations: [],
       title: {},
       description: {}
-    }))
+    }, `instance`, `requires property "localizationName"`))
     shared.testLocalizedString(schema.schema, instance => ({
       localizations: [],
       localizationName: instance,
       title: {},
       description: {}
-    }))
+    }), `instance.localizationName`)
   })
   describe(`title`, () => {
     describe(`missing`, () => shared.rejects(schema.schema, {
       localizations: [],
       localizationName: {},
       description: {}
-    }))
+    }, `instance`, `requires property "title"`))
     shared.testLocalizedString(schema.schema, instance => ({
       localizations: [],
       localizationName: {},
       title: instance,
       description: {}
-    }))
+    }), `instance.title`)
   })
   describe(`description`, () => {
     describe(`missing`, () => shared.rejects(schema.schema, {
       localizations: [],
       localizationName: {},
       title: {}
-    }))
+    }, `instance`, `requires property "description"`))
     shared.testLocalizedString(schema.schema, instance => ({
       localizations: [],
       localizationName: {},
       title: {},
       description: instance
-    }))
+    }), `instance.description`)
   })
 })
