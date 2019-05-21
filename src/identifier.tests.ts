b31afa9c3295c0d26815c3e074ffc9f0dd3f3285
jameswilddev <jameswilddev@outlook.com>
2019-05-21 20:42:23 +0100

    Refactored existing tests to make testing of common types reusable.

--- src/identifier.tests.ts
+++ src/identifier.tests.ts
@@ -3,7 +3,5 @@ import * as identifier from "./identifier"
 import * as shared from "./shared.tests"
 
 describe(`identifier`, () => {
-  shared.run(shared.exhaustiveIdentifierStrings, value => shared.accepts(identifier.schema, value))
-  shared.run(shared.nonIdentifierStrings, value => shared.rejects(identifier.schema, value))
-  shared.run(shared.nonStrings, value => shared.rejects(identifier.schema, value))
+  shared.testIdentifier(identifier.schema, instance => instance)
 })
