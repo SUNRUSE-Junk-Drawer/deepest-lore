03a5278bb422b1c3aecbbf815559bf8f6eceaa56
jameswilddev <jameswilddev@outlook.com>
2019-05-28 01:12:21 +0100

    Added entity type sets (#1).

---
+++ src/entity-type-set.tests.ts
@@ -0,0 +1,7 @@
+import "jasmine"
+import * as entityTypeSet from "./entity-type-set"
+import * as shared from "./shared.tests"
+
+describe(`entity type set`, () => {
+  shared.testEntityTypeSet(entityTypeSet.schema, value => value, `instance`)
+})
